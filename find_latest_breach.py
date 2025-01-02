762dca76b158ed316550afe6f8ccfd9193632f4f
User-Seis-PNSN <seis@proc02-prd-a146.pnsn.uw.edu>
2025-01-02 15:10:54 -0800

    added DH chantype and guestimated its threshold

--- find_latest_breach.py
+++ find_latest_breach.py
@@ -37,7 +37,7 @@ except Exception as e:
 #   - metric_id = 101 => “5s power”
 chantypes = ["EN", "HN", "HH", "BH", "EH", "BD", "DH"]
 thresh_range = [20, 200, 550, 650, 50, 29000, 200]  # For metric_id=85
-thresh_5s_power = [-122.6, -123.0, -130.3, -129.8, -150.0, -39.0]  # For metric_id=101
+thresh_5s_power = [-122.6, -123.0, -130.3, -129.8, -150.0, -39.0, -130.0]  # For metric_id=101
 
 DEFAULT_THRESHDICT = {
     85: dict(zip(chantypes, thresh_range)),
